2301b42931802fb76288c2b1b3af43fc36a559af
solemnwarning <solemnwarning@8eaceb45-6f80-42b0-b3a5-e78a358d2bcd>
2010-08-10 00:28:07 +0000

    Wrote code and chewed bubble gum (not really)
    Epic tile graphic from scgtrp
    
    
    git-svn-id: http://solemnwarning.net/svn/octradius/trunk@3 8eaceb45-6f80-42b0-b3a5-e78a358d2bcd

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,26 @@
 #include <SDL/SDL_image.h>
 #include <unistd.h>
 #include <assert.h>
+#include <vector>
+
+namespace OctRadius {
+	class Tile {
+		public:
+			int row;
+			int col;
+			
+			int height;
+			
+			Tile(int r, int c) {
+				row = r;
+				col = c;
+				height = 0;
+			}
+	};
+}
+
+typedef std::vector<OctRadius::Tile> tile_list;
+typedef std::vector<tile_list> tile_table;
 
 int main(int argc, char **argv) {
 	if(argc != 3) {
@@ -13,6 +33,19 @@ int main(int argc, char **argv) {
 	
 	int width = atoi(argv[1]), height = atoi(argv[2]);
 	
+	tile_table tiles;
+	int row, col;
+	
+	for(row = 0; row < height; row++) {
+		tile_list rlist;
+		
+		for(col = 0; col < width; col++) {
+			rlist.push_back(OctRadius::Tile(row, col));
+		}
+		
+		tiles.push_back(rlist);
+	}
+	
 	assert(SDL_Init(SDL_INIT_VIDEO) == 0);
 	
 	SDL_Surface *screen = SDL_SetVideoMode(width*100, height*100, 0, SDL_SWSURFACE);
@@ -20,17 +53,22 @@ int main(int argc, char **argv) {
 	
 	SDL_WM_SetCaption("OctRadius", "OctRadius");
 	
-	SDL_Surface *square = IMG_Load("graphics/square.png");
+	SDL_Surface *square = IMG_Load("graphics/tile.png");
 	assert(square != NULL);
 	
-	int w, h;
-	
-	for(w = 0; w < width; w++) {
-		for(h = 0; h < height; h++) {
+	for(row = 0; row < height; row++) {
+		for(col = 0; col < width; col++) {
 			SDL_Rect rect;
-			rect.x = 100*w;
-			rect.y = 100*h;
+			rect.x = 100*col;
+			rect.y = 100*row;
 			rect.w = rect.h = 0;
+			
+			if(row == col) {
+				tiles[row][col].height += 2;
+			}
+			
+			rect.x += (-1 * tiles[row][col].height) * 10;
+			rect.y += (-1 * tiles[row][col].height) * 10;
 			
 			assert(SDL_BlitSurface(square, NULL, screen, &rect) == 0);
 		}
