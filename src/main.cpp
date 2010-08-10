1361aa4587b26ace261892fb940133a0f287bf6e
solemnwarning <solemnwarning@8eaceb45-6f80-42b0-b3a5-e78a358d2bcd>
2010-08-10 01:57:56 +0000

    Updated board rendering code, added buggy support for raising/lowering tiles with the scroll wheel
    git-svn-id: http://solemnwarning.net/svn/octradius/trunk@6 8eaceb45-6f80-42b0-b3a5-e78a358d2bcd

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,53 @@
 namespace OctRadius {
 	class Tile {
 		public:
-			int row;
-			int col;
-			
+			int col, row;
 			int height;
 			
-			Tile(int r, int c) {
-				row = r;
-				col = c;
-				height = 0;
-			}
+			int screen_x, screen_y;
+			
+			Tile(int c, int r) : col(c), row(r), height(0) {}
 	};
 }
 
 typedef std::vector<OctRadius::Tile> tile_list;
 typedef std::vector<tile_list> tile_table;
+
+namespace OctRadius {
+	void DrawBoard(tile_table &tiles, SDL_Surface *screen);
+}
+
+#define TILE_SIZE 100
+#define BOARD_OFFSET 20
+
+void OctRadius::DrawBoard(tile_table &tiles, SDL_Surface *screen) {
+	int cols = tiles.size();
+	int rows = tiles[0].size();
+	
+	SDL_Surface *square = IMG_Load("graphics/tile.png");
+	assert(square != NULL);
+	
+	assert(SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0, 0, 0)) != -1);
+	
+	for(int r = 0; r < rows; r++) {
+		for(int c = 0; c < cols; c++) {
+			SDL_Rect rect;
+			rect.x = BOARD_OFFSET + TILE_SIZE * c;
+			rect.y = BOARD_OFFSET + TILE_SIZE * r;
+			rect.w = rect.h = 0;
+			
+			rect.x += (-1 * tiles[c][r].height) * 10;
+			rect.y += (-1 * tiles[c][r].height) * 10;
+			
+			tiles[c][r].screen_x = rect.x;
+			tiles[c][r].screen_y = rect.y;
+			
+			assert(SDL_BlitSurface(square, NULL, screen, &rect) == 0);
+		}
+	}
+	
+	SDL_UpdateRect(screen, 0,0,0,0);
+}
 
 int main(int argc, char **argv) {
 	if(argc != 3) {
@@ -36,11 +68,11 @@ int main(int argc, char **argv) {
 	tile_table tiles;
 	int row, col;
 	
-	for(row = 0; row < height; row++) {
+	for(col = 0; col < width; col++) {
 		tile_list rlist;
 		
-		for(col = 0; col < width; col++) {
-			rlist.push_back(OctRadius::Tile(row, col));
+		for(row = 0; row < height; row++) {
+			rlist.push_back(OctRadius::Tile(col, row));
 		}
 		
 		tiles.push_back(rlist);
@@ -48,33 +80,12 @@ int main(int argc, char **argv) {
 	
 	assert(SDL_Init(SDL_INIT_VIDEO) == 0);
 	
-	SDL_Surface *screen = SDL_SetVideoMode(width*100, height*100, 0, SDL_SWSURFACE);
+	SDL_Surface *screen = SDL_SetVideoMode((width*TILE_SIZE) + (2*BOARD_OFFSET), (width*TILE_SIZE) + (2*BOARD_OFFSET), 0, SDL_SWSURFACE);
 	assert(screen != NULL);
 	
 	SDL_WM_SetCaption("OctRadius", "OctRadius");
 	
-	SDL_Surface *square = IMG_Load("graphics/tile.png");
-	assert(square != NULL);
-	
-	for(row = 0; row < height; row++) {
-		for(col = 0; col < width; col++) {
-			SDL_Rect rect;
-			rect.x = 100*col;
-			rect.y = 100*row;
-			rect.w = rect.h = 0;
-			
-			if(row == col) {
-				tiles[row][col].height += 2;
-			}
-			
-			rect.x += (-1 * tiles[row][col].height) * 10;
-			rect.y += (-1 * tiles[row][col].height) * 10;
-			
-			assert(SDL_BlitSurface(square, NULL, screen, &rect) == 0);
-		}
-	}
-	
-	SDL_UpdateRect(screen, 0,0,0,0);
+	OctRadius::DrawBoard(tiles, screen);
 	
 	SDL_Event event;
 	
@@ -82,6 +93,35 @@ int main(int argc, char **argv) {
 		if(SDL_PollEvent(&event)) {
 			if(event.type == SDL_QUIT) {
 				break;
+			}
+			
+			if(event.type == SDL_MOUSEBUTTONDOWN) {
+				std::cout << "Mouse #" << (int)event.button.button << " pressed at " << event.button.x << "," << event.button.y << std::endl;
+				
+				for(int c = 0; c < width; c++) {
+					for(int r = 0; r < height; r++) {
+						int tile_x = tiles[c][r].screen_x;
+						int tile_y = tiles[c][r].screen_y;
+						
+						if(tile_x <= event.button.x && tile_x+TILE_SIZE > event.button.x && tile_y <= event.button.y && tile_y+TILE_SIZE > event.button.y) {
+							std::cout << "Matched tile: " << tiles[c][r].col << "," << tiles[c][r].row << std::endl;
+							
+							if(event.button.button == SDL_BUTTON_WHEELUP) {
+								std::cout << "Raising tile" << std::endl;
+								
+								tiles[c][r].height++;
+								OctRadius::DrawBoard(tiles, screen);
+							}
+							
+							if(event.button.button == SDL_BUTTON_WHEELDOWN) {
+								std::cout << "Lowering tile" << std::endl;
+								
+								tiles[c][r].height--;
+								OctRadius::DrawBoard(tiles, screen);
+							}
+						}
+					}
+				}
 			}
 		}
 	}
