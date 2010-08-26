694a878567ca35d825bd1eb36ae9ee124740e17d
solemnwarning <solemnwarning@8eaceb45-6f80-42b0-b3a5-e78a358d2bcd>
2010-08-26 03:11:58 +0000

    Moved menu graphics around
    git-svn-id: http://solemnwarning.net/svn/octradius/trunk@168 8eaceb45-6f80-42b0-b3a5-e78a358d2bcd

--- src/menu.cpp
+++ src/menu.cpp
@@ -35,11 +35,11 @@ static void join_cb() {
 }
 
 MainMenu::MainMenu() : gui(0, 0, MENU_WIDTH, MENU_HEIGHT) {
-	gui.set_bg_image(ImgStuff::GetImage("graphics/menu_background.png"));
+	gui.set_bg_image(ImgStuff::GetImage("graphics/menu/background.png"));
 	
-	join_btn = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menus/join_game.png"), 350, 300, 2, &main_join_cb);
-	host_btn = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menus/host_game.png"), 350, 250, 1, &main_host_cb);
-	quit_btn = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menus/quit.png"), 700, 570, 3, &quit_cb);
+	join_btn = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menu/join_game.png"), 350, 300, 2, &main_join_cb);
+	host_btn = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menu/host_game.png"), 350, 250, 1, &main_host_cb);
+	quit_btn = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menu/quit.png"), 700, 570, 3, &quit_cb);
 }
 
 MainMenu::~MainMenu() {
@@ -58,21 +58,21 @@ void MainMenu::run() {
 }
 
 JoinMenu::JoinMenu() : gui(0, 0, MENU_WIDTH, MENU_HEIGHT) {
-	gui.set_bg_image(ImgStuff::GetImage("graphics/menu_background.png"));
+	gui.set_bg_image(ImgStuff::GetImage("graphics/menu/background.png"));
 	
-	host_label = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menus/host.png"), 315, 250, 100, NULL);
+	host_label = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menu/host.png"), 315, 250, 100, NULL);
 	host_label->enable(false);
 	
 	host_input = new GUI::TextBox(gui, 375, 255, 200, 20, 1);
 	
-	port_label = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menus/port.png"), 325, 300, 101, NULL);
+	port_label = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menu/port.png"), 325, 300, 101, NULL);
 	port_label->enable(false);
 	
 	port_input = new GUI::TextBox(gui, 375, 305, 200, 20, 2);
 	
-	join_btn = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menus/join_game.png"), 350, 350, 3, &join_cb);
-	back_btn = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menus/back.png"), 0, 570, 4, &back_cb);
-	quit_btn = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menus/quit.png"), 700, 570, 5, &quit_cb);
+	join_btn = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menu/join_game.png"), 350, 350, 3, &join_cb);
+	back_btn = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menu/back.png"), 0, 570, 4, &back_cb);
+	quit_btn = new GUI::ImgButton(gui, ImgStuff::GetImage("graphics/menu/quit.png"), 700, 570, 5, &quit_cb);
 }
 
 JoinMenu::~JoinMenu() {
@@ -98,19 +98,19 @@ void JoinMenu::run() {
 
 #if 0
 		host_menu.screen = screen;
-		host_menu += new Button(325, 250, 40, 30, "graphics/menus/port.png", NULL);
+		host_menu += new Button(325, 250, 40, 30, "graphics/menu/port.png", NULL);
 		host_port_box = new TextInput(375, 255, 200, 20, "9001", 14, textbox_fg, textbox_bg);
 		host_menu += host_port_box;
-		host_menu += new Button(275, 300, 90, 30, "graphics/menus/scenario.png", NULL);
+		host_menu += new Button(275, 300, 90, 30, "graphics/menu/scenario.png", NULL);
 		host_scenario_box = new TextInput(375, 305, 200, 20, "scenario/hex_2p.txt", 14, textbox_fg, textbox_bg);
 		host_menu += host_scenario_box;
-		host_menu += new Button(350, 350, 100, 30, "graphics/menus/host_game.png", start_server_callback);
-		host_menu += new Button(0, 570, 100, 30, "graphics/menus/back.png", return_callback);
-		host_menu += new Button(700, 570, 100, 30, "graphics/menus/quit.png", quit_callback);
+		host_menu += new Button(350, 350, 100, 30, "graphics/menu/host_game.png", start_server_callback);
+		host_menu += new Button(0, 570, 100, 30, "graphics/menu/back.png", return_callback);
+		host_menu += new Button(700, 570, 100, 30, "graphics/menu/quit.png", quit_callback);
 		host_menu.background = main_menu.background;
 		
 		wait_for_players_menu.screen = screen;
-		wait_for_players_menu += new Button(300, 250, 40, 30, "graphics/menus/waiting_for_players.png", NULL);
+		wait_for_players_menu += new Button(300, 250, 40, 30, "graphics/menu/waiting_for_players.png", NULL);
 		wait_for_players_menu.background = main_menu.background;
 		wait_for_players_menu.do_stuff = host_do_stuff;
 #endif
