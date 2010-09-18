42768e28f758767e84466974fab1eac81c7d85bd
solemnwarning <solemnwarning@8eaceb45-6f80-42b0-b3a5-e78a358d2bcd>
2010-09-18 19:56:58 +0000

    Fix to make it compile under win32
    git-svn-id: http://solemnwarning.net/svn/octradius/trunk@223 8eaceb45-6f80-42b0-b3a5-e78a358d2bcd

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,11 @@
 #include <math.h>
 
 #ifdef _WIN32
-#include <windows.h>
+/* Including windows.h here makes it error when ASIO includes winsock.h since
+ * windows is Retarded.
+*/
+
+// #include <windows.h>
 #endif
 
 #include "loadimage.hpp"
