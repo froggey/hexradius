739af041fb7a9afa928403889d61384c0125f6f9
Henry Harrington <henry.harrington@gmail.com>
2013-09-21 02:01:34 +0100

    Accidentally negated a condition while removing HAS_POWER.
    This caused all pawns to be rendered with their power lamp running
    when they had no powers and it not running when they had powers.

--- src/client.cpp
+++ src/client.cpp
@@ -781,7 +781,7 @@ void Client::DrawPawn(pawn_ptr pawn, SDL_Rect rect, SDL_Rect base, const std::se
 
 		if(pawn == hpawn && pawn->colour == my_colour) {
 			frame = 10;
-		} else if(pawn->has_power()) {
+		} else if(!pawn->has_power()) {
 			frame = 0;
 		}
 
